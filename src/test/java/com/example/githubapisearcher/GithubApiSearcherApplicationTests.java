bdb05e06c7d315ca0d7f96b3d630400ce2e2640f
ppirog <pawelpirog99@gmail.com>
2023-11-02 17:16:43 +0100

    Initial commit

---
+++ src/test/java/com/example/githubapisearcher/GithubApiSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapisearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiSearcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
